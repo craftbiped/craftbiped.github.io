ed871c2b0898b86090906bfa339ba0693f0de0ff
CraftBiped <155679909+craftbiped@users.noreply.github.com>
2024-04-06 00:00:00 +0300

    Update tech.js

--- tech.js
+++ tech.js
@@ -1,1 +1,6 @@
-window.location.replace("https://craftbiped.github.io/tech");
+var d = new Date();
+var day=new Array("Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота");
+var month=new Array("января","февраля","марта","апреля","мая","июня","июля","августа",
+"сентября","октября","ноября","декабря");
+document.write(day[d.getDay()]+" " +d.getDate()+ " " + month[d.getMonth()]+ " 
+" + d.getFullYear() + " г.");
