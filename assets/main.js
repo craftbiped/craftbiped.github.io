76684dc46681eddac98a5b0879b6757c658a8d16
CraftBiped <125664555+craftbiped@users.noreply.github.com>
2025-01-29 19:59:19 +0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -118,3 +118,15 @@ if (window.location.pathname == "/") {
 } else {
 console.log('Режим приложения выключен');
 }
+
+
+  if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register('/service-worker.js').then(function () {
+      console.log('CLIENT: service worker registration complete.');
+    }, function () {
+      console.log('CLIENT: service worker registration failure.');
+    });
+  } else {
+    console.log('CLIENT: service worker is not supported.');
+  }
