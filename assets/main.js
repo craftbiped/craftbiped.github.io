7075b33944200b874fdafc39167495d590271e9b
CraftBiped <125664555+craftbiped@users.noreply.github.com>
2025-03-08 00:55:22 +0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -157,7 +157,7 @@ if (ccode == 'RU') {
 var socialss = document.getElementById("insocials");
 socialss.innerHTML = `
 <center>
-<h1>Мы в соц. сетях</h1>
+<h1>Мы в <s>соц. сетях</s>мессенджерах</h1>
 <br>
 <h3>Мы в Discord</h3>
 <p style="color: red;">Discord заблокирован в вашей стране, однако, если у вас работает дискорд, вы можете присоединиться к нашему Discord-серверу!</p>
