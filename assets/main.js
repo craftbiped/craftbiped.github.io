309bfdbc7f3091d078cb696eb168540f97fc8d8f
CraftBiped <125664555+craftbiped@users.noreply.github.com>
2025-04-02 22:29:12 +0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -47,6 +47,7 @@ menumenu.innerHTML = `
 <li><a href="/support">ПОДДЕРЖКА</a></li>
 <li><a href="/ds">НАШ DISCORD</a></li>
 <li><a href="https://t.me/craftbipedmc">НАШ TELEGRAM</a></li>
+<li><a href="/status">СТАТУС СЕРВЕРА</a></li>
 </ul>
 <h2 id="mobilemenu"><a onclick="openmenu()" id="openmenubutton" class="btn" style="color: white; text-decoration: none;">☰ меню</a></h2>
 <div id="menulist">
@@ -76,6 +77,7 @@ cop.innerHTML = `
 <p><a href="/support" class="bottom-element">Поддержка</a></p>
 <p><a href="/ds" class="bottom-element">Наш Discord</a></p>
 <p><a href="https://t.me/craftbipedmc" class="bottom-element">Наш Telegram</a></p>
+<p><a href="/status" class="bottom-element">Статус сервера</a></p>
 
 <br><br>
 <div id="countr"><img src="/assets/loading.gif" width="35" height="35"></div>
@@ -123,9 +125,11 @@ function openmenu(){
 <br>
 <li><a href="/sets/"> НАБОРЫ</a></li>
 <br>
-<li><a href="/ds">НАШ DISCORD</a></li>
+<li><a href="/ds"> НАШ DISCORD</a></li>
 <br>
-<li><a href="https://t.me/craftbipedmc">НАШ TELEGRAM</a></li>
+<li><a href="https://t.me/craftbipedmc"> НАШ TELEGRAM</a></li>
+<br>
+<li><a href="/status"> СТАТУС СЕРВЕРА</a></li>
 <br><br><br>
 </center>
     `;
