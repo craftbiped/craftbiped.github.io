08a56f84cd58e77cb4a06f1565658a76a78ffc9d
CraftBiped <125664555+craftbiped@users.noreply.github.com>
2025-04-23 10:26:09 +0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,7 @@ jjs.innerHTML = `
 var menumenu = document.getElementById("menu");
 menumenu.innerHTML = `
 <!--<div id="jsnof" style="background-color: green;">
-<!--<p>Наш IP: craftbiped.mc.land <!--<a href="https://craftbiped.github.io/blog/news/2025/02/14/new-ip.html">Подробнее...</a>--></p>-->
+<p>Наш IP: craftbiped.mc.land <a href="https://craftbiped.github.io/blog/news/2025/02/14/new-ip.html">Подробнее...</a></p>
  </p>
 </div>-->
 <br>
