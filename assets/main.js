ff7da9deb3120b69466c9eb101e293add8c90235
CraftBiped <125664555+craftbiped@users.noreply.github.com>
2025-06-23 19:57:13 +0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -181,6 +181,9 @@ socialss.innerHTML = `
 </center>
 `;
 }
+if (ccode == 'IL') {
+document.body.innerHTML = "<h1>В данный момент CraftBiped не работает в вашей стране.</h1>"
+}
 if (ccode == 'UA') {
 var socialss = document.getElementById("insocials");
 socialss.innerHTML = `
